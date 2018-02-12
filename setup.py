44444872bb2587f4f4fdff54e6fc70511060bf07
ray <gomyar@gmail.com>
2018-02-12 12:45:43 +0000

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+requirements = open("requirements.txt")
+
+setup(
+    name='metaphor',
+    version='0.1',
+    description='API Layer for MongoDB.',
+    packages=find_packages("src"),
+    include_package_data=True,
+    package_dir= { '': 'src'},
+    install_requires=[req.strip() for req in requirements],
+)
