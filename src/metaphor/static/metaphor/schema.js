14b2f73e67409eafb21ef1d1ccafee0350ff2d1a
ray <gomyar@gmail.com>
2018-04-29 15:00:54 +0000

    Add a spec through gui

--- src/metaphor/static/metaphor/schema.js
+++ src/metaphor/static/metaphor/schema.js
@@ -12,20 +12,11 @@ gui.workingfield = {
     'calc': null,
     'calc_type': null,
 };
+gui.working_spec = {
+    'name': null
+};
 gui.field_types = ['str', 'int', 'float', 'collection', 'link', 'calc'];
 gui.calc_types = ['str', 'int', 'float'];
-
-gui.add_field = function(field_name, field_type) {
-}
-
-gui.add_calc = function(field_name, calc, calc_type) {
-}
-
-gui.add_link = function(field_name, target) {
-}
-
-gui.add_collection = function(field_name, target) {
-}
 
 gui.delete_field = function(spec_name, field_name) {
     if (confirm('Are you sure you want to delete ' + field_name + '?')) {
@@ -37,8 +28,11 @@ gui.load_specs = function(onload) {
     net.perform_get("/schema/specs", function(data) {
         schema = data;
         turtlegui.reload();
-        if (onload)
+        if (onload) {
             onload();
+        }
+    }, function(errorText, jqXHR) {
+        alert("Error: " + jqXHR.responseText);
     });
 }
 
@@ -58,8 +52,19 @@ gui.create_field = function() {
                 field_div.hide();
                 field_div.fadeIn();
             });
+        }, function(errorText, jqXHR) {
+        alert("Error: " + jqXHR.responseText);
+    });
+}
+
+gui.create_spec = function() {
+    var spec_data = {'name': gui.working_spec.name};
+    net.perform_post("/schema/specs",
+        spec_data, function(e) {
+            gui.load_specs();
         });
 }
+
 
 
 $(document).ready(function() {
