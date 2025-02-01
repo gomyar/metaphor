39a7b8c67285cf4f9d6cd7ba3c46115223427518
ray <ray@stackturtle>
2025-02-01 18:40:15 +0000

    Add missing file and small template update

---
+++ src/metaphor/update/mutation_alter_field.py
@@ -0,0 +1,35 @@
+
+
+class AlterFieldMutation:
+    def __init__(self, mutation, schema, spec_name, field_name, new_type):
+        self.mutation = mutation
+        self.schema = schema
+
+        self.spec_name = spec_name
+        self.field_name = field_name
+        self.new_type = new_type
+
+    def __repr__(self):
+        return "<AlterFieldTypeConvertPrimitiveMutation>"
+
+    def actions(self):
+        return None
+
+    def execute(self, action=None):
+        update_id = str(self.schema.create_update())
+
+        spec = self.schema.specs[self.spec_name]
+        from_field = self.schema.specs[self.spec_name].fields[self.field_name]
+        to_field = self.mutation.to_schema.specs[self.spec_name].fields[self.field_name]
+
+        if from_field.field_type != to_field.field_type:
+            self.schema.alter_field_convert_type(self.spec_name, self.field_name, self.new_type)
+
+        if from_field.indexed != to_field.indexed or from_field.unique != to_field.unique or from_field.unique_global != to_field.unique_global:
+            if from_field.indexed or from_field.unique or from_field.unique_global:
+                self.schema.drop_index_for_field(self.spec_name, self.field_name)
+            self.schema.create_index_for_field(self.spec_name, self.field_name, to_field.unique, to_field.unique_global)
+
+        self.mutation.updater.update_for_field(self.spec_name, self.field_name, update_id, [])
+
+        self.schema.cleanup_update(update_id)
